7418e90532f53afcd10db13812b917a13fdc3367
abzerouali <abderrahmanezerouali98@gmail.com>
2023-06-15 05:30:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,14 @@
  *input/output operations library for C programming language
  */
 #include <unistd.h>
-
 /**
- * main - Entry point
- * Return: 1 (Success):wq
+ * main - Entry point of the program
  *
+ * Description: main
+ * Return: Always 1 
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
