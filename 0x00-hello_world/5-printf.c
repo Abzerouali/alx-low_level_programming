7e2d8f4ba8b781e636087aa7e32d61e49910ee82
abzerouali <abderrahmanezerouali98@gmail.com>
2023-06-15 04:58:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,8 @@
  *   *
  *    * Return: 0 (success)
  *     */
-int main(void)
-{
-	    printf("with proper grammar, but the outcome is a piece of art,\n");
-	        return (0);
+	int main(voi)
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
 
