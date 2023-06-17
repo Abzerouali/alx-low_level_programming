3923377515cd1afa300d8597e6892b1481458557
abzerouali <abderrahmanezerouali98@gmail.com>
2023-06-16 19:32:11 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: all the numbers of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+		int N, A;
+
+		while (N = 0; N <= 9)
+		{
+			putchar(N);
+			N++;
+		}
+		while (A = a ; A < 'g')
+		{
+			putchar(A);
+			A++;
+		}
+		putchar('\n');
+		return (0);
+}
