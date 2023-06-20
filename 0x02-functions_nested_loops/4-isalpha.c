5b7a3f7e0e77c6fa7d1266f713dd39f6ac75cea3
abzerouali <abderrahmanezerouali98@gmail.com>
2023-06-20 10:41:59 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,12 +8,8 @@
 
 int _isalpha(int c)
 {
-	for (;(c > 96 && c < 123) || (c > 64 && c < 91);)
-	{
+	if ((c > 96 && c < 123) || (c > 64 && c < 91))
 		return (1);
-		for (;!((c > 96 && c < 123) || (c > 64 && c < 91));)
-		{
-			return (0);
-		}
-	}
+	else
+		return (0);
 }
