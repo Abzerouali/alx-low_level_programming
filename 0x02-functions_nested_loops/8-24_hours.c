9cfcd03efbea70f5b0c83451a8d1eb02094eea41
abzerouali <abderrahmanezerouali98@gmail.com>
2023-06-20 14:19:06 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,6 +21,7 @@ void jack_bauer(void)
 					_putchar(':');
 					_putchar(c + '0');
 					_putchar(d + '0');
+					_putchar('\n');
 				}
 			}
 		}
@@ -38,6 +39,7 @@ void jack_bauer(void)
 					_putchar(':');
 					_putchar(c + '0');
 					_putchar(d + '0');
+					_putchar('\n');
 				}
 			}
 		}
