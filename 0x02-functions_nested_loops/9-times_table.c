02ea7a62af82dccaeceb0841a7814dbbfbebc8e7
abzerouali <abderrahmanezerouali98@gmail.com>
2023-06-20 17:20:05 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -34,11 +34,11 @@ void times_table(void)
 						_putchar(' ');
 						_putchar(' ');
 						_putchar(c + '0');
+						if (b < 9)
+						{
+							_putchar(',');
+						}
 					}
-				}
-				if (b < 9)
-				{
-					_putchar(',');
 				}
 			}
 			else
