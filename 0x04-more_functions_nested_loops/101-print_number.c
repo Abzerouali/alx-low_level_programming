34c6d4ea802239f603c9b9a2b83e4c09f6d80fc9
abzerouali <abderrahmanezerouali98@gmail.com>
2023-06-22 15:57:12 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer
+ * @n: integer
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		print_number(-n);
+	}
+	else if (n <= 9)
+	{
+		_putchar(n + '0');
+		print_number(n);
+	}
+
+	else
+	{
+		print_number(n / 10);
+		print_number(n % 10);
+	}
+}
