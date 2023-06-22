b52e0db5077adf7b7e9b711ad17ffcf351090a72
abzerouali <abderrahmanezerouali98@gmail.com>
2023-06-22 13:55:58 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,30 +8,30 @@
 
 int main(void)
 {
-	int a = 1;
+	int a;
 
-	do {
-			if (a % 3 == 0 && a % 5 == 0)
-			{
-				printf("FizzBuzz");
-			}
-			else if (a % 5 == 0)
-			{
-				printf("Buzz");
-			}
-
-			else if (a % 3 == 0)
-			{
-				printf("Fizz");
-			}
-			else
-			{
-				printf("%d", a);
-			}
+	while (a = 1; a <= 100; a++)
+       	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", a);
+		}
 		if (a != 100)
 			printf(" ");
-		a++;
-	} while (a <= 100);
+	
+	}
 	printf("\n");
 	return (0);
 }
