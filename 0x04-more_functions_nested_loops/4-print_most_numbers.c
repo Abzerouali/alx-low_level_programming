a3ac3193e31aaa492a9074f8267cd606fe59fd41
abzerouali <abderrahmanezerouali98@gmail.com>
2023-06-22 09:55:05 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+/**
+ * * print_most_numbers - a function that prints the numbers, from 0 to 9
+ */
+
+void print_most_numbers(void)
+{
+		int a;
+
+		a = 0;
+		while (a < 10)
+		{
+			if (a == 2 || a == 4)
+			{
+				continue;
+			}
+			_putchar(a + '0');
+			a++;
+		}
+		_putchar('\n');
+}
