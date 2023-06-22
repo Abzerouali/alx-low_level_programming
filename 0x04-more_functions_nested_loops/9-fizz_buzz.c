e3c264dd3ebe9654cee5a645f1cbe017682b8201
abzerouali <abderrahmanezerouali98@gmail.com>
2023-06-22 13:59:43 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 	int a;
 
 	for (a = 1; a <= 100; a++)
-       	{
+	{
 		if (a % 3 == 0 && a % 5 == 0)
 		{
 			printf("FizzBuzz");
@@ -30,7 +30,7 @@ int main(void)
 		}
 		if (a != 100)
 			printf(" ");
-	
+
 	}
 	printf("\n");
 	return (0);
