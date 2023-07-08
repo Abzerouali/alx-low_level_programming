8e727ead0378c2b8c47173aa9b09c0ca4c8e34b3
abzerouali <abderrahmanezerouali98@gmail.com>
2023-07-07 18:16:06 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,29 +9,29 @@
 *@argv: argument vector
 *Return: 0
 */
-
 int main(int argc, char **argv)
 {
-int x, y, sum = 0;
+	
+	int x, y, sum = 0;
 
-if (argc == 1)
-{
-	printf("0\n");
-	return (0);
-}
-
-for (x = 1; x < argc; x++)
-{
-	for (y = 0; argv[x][y] != '\0'; y++)
+	if (argc == 1)
 	{
-		if (!((argv[x][y] >= '0' && argv[x][y] < '9') || argv[x][y] == '-'))
-		{
-			printf("Error\n");
-			return (1);
-		}
+		printf("0\n");
+		return (0);
 	}
-	sum += atoi(argv[x]);
-}
-printf("%d\n", sum);
-return (0);
+
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (!((argv[x][y] >= '0' && argv[x][y] <= '9') || argv[x][y] == '-'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[x]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
