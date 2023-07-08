3d34880a9cd8c6f96eabcbbc0b2cdd1e221244f1
abzerouali <abderrahmanezerouali98@gmail.com>
2023-07-07 17:57:37 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,18 +2,16 @@
 #include <stdio.h>
 
 /**
- *
- * str_len - Prototype
- * Description: a function calculate the length of a string
- * @s: string
- * Return: len
- */
+*str_len - Prototype
+*Description: a function calculate the length of a string
+*@s: string
+*Return: len
+*/
 
 int str_len(char *s)
 {
-	int len;
+	int len = 0;
 
-	len = 0;
 	while (s[len])
 		len++;
 	return (len);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 int _atoi(char *s);
+int str_len(char *s)
 
 #endif
