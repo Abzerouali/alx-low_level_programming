901e98e20214253a94949d23860f0321a4285e2e
abzerouali <abderrahmanezerouali98@gmail.com>
2023-07-18 06:38:04 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_LAB
 #define DOG_LAB
-
+/**
+ * struct dog - dog's name, age, and owner's name.
+ *@name: Pointer to the dog's name.
+ *@age: Age of the dog.
+ *@owner: Pointer to the owner's name.
+ */
 struct dog
 {
 	char *name;
