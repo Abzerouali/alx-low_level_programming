959f8558ddd001943b5978e790596bddc27d6942
abzerouali <abderrahmanezerouali98@gmail.com>
2023-07-19 05:42:27 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,9 +6,8 @@
 *@array: The array to search.
 *@size: The number of elements in the array.
 *@cmp:  a pointer to the function to be used to compare values.
-*Returns:
-*The index of the first occurrence of the value that satisfies the
-comparison function, or -1 if the value is not found.
+*Returns:the index of the first occurrence of the value that satisfies
+*the comparison function, or -1 if the value is not found.
 */
 
 int int_index(int *array, int size, int (*cmp)(int))
