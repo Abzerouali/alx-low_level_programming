b02129ff0b54a35c17ac8c6848bf5fd838defbca
Abderrahmane Zerouali <abderrahmanezerouali98@gmail.com>
2023-08-08 15:40:54 -0700

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,12 +4,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <elf.h>
 
 
 size_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void close_f(int fd);
-char *_buffer(char *f);
 
 #endif
